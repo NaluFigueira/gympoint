b4720b64d9377b8bd9e92164fe452016cd1db1f8
NaluFigueira <anachfigueira@gmail.com>
2019-10-29 23:52:01 -0300

    Adicionando tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,23 +1,41 @@
 import express, { json } from 'express';
 
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import youch from 'youch';
+import sentryConfig from './config/sentry'
 import routes from './routes';
 
 import './database';
 
+
+
 class App {
   constructor() {
     this.server = express();
+    
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptionHandler(){
+    this.server.use( async(err,req,res,next) => {
+      const errors = await new youch(err, req).toJSON();
+      return res.status(500).json(err);
+    })
   }
 }
 
